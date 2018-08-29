19e46f7d701483fcbae64557c243c00b95b793ce
Alex H <alexandra.higuera@gmail.com>
2018-08-29 13:02:56 -0400

    Add react-player

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ReactPlayer from 'react-player';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
 
@@ -12,8 +13,12 @@ export default class Home extends Component<Props> {
   render() {
     return (
       <div className={styles.container} data-tid="container">
-        <h2>Neurolearning</h2>
+        <h3>Neurolearning</h3>
         <h3>Video Container</h3>
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=_3x6_doymmw"
+          playing
+        />
         <Link to={routes.COUNTER}>to Counter</Link>
       </div>
     );
