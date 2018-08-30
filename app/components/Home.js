d7b7f43116667a261a74e97859a2e19db4389867
Alex H <alexandra.higuera@gmail.com>
2018-08-29 20:38:27 -0400

    Remove react-player

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,7 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import ReactPlayer from 'react-player';
+
 import routes from '../constants/routes.json';
 import styles from './Home.css';
 
@@ -15,10 +15,7 @@ export default class Home extends Component<Props> {
       <div className={styles.container} data-tid="container">
         <h3>Neurolearning</h3>
         <h3>Video Container</h3>
-        <ReactPlayer
-          url="https://www.youtube.com/watch?v=_3x6_doymmw"
-          playing
-        />
+
         <Link to={routes.COUNTER}>to Counter</Link>
       </div>
     );
