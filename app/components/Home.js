d654c4082b0574a3893cc624e923ff4477527848
Alex H <alexandra.higuera@gmail.com>
2018-09-25 18:18:13 -0400

    New VideoSet component and routing

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,8 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
-import { Button, Modal } from 'semantic-ui-react';
+import { Input } from 'semantic-ui-react';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
 
@@ -11,84 +10,41 @@ type Props = {};
 export default class Home extends Component<Props> {
   props: Props;
 
-  state = { openModal: false };
-
-  close = () => this.setState({ openModal: false });
-
-  open = () => this.setState({ openModal: true });
-
   render() {
-    const { openModal } = this.state;
-
     return (
       <div className={styles.container} data-tid="container">
-        <h3>Neurolearning</h3>
-        <h3>Video Container</h3>
+        <h2>Neurolearning Project</h2>
+
         <div>
-          <video
-            className={styles.video}
-            width="70%"
-            height="70%"
-            src="../app/Bip_KC_Trim.mp4"
-            controls
-          >
-            <track kind="captions" {...Props} />
-          </video>
+          <p>Welcome</p>
         </div>
-
-        <Modal
-          open={openModal}
-          className={styles.modal}
-          trigger={<Button onClick={this.open}>Show Modal</Button>}
-          closeOnEscape={false}
-          closeOnDimmerClick={false}
-          onClose={this.close}
-        >
-          <div className={styles.inner}>
-            <Modal.Header>Popup Question #1</Modal.Header>
-            <Modal.Content className={styles.content}>
-              <Modal.Description>
-                <p>
-                  Which one of the following is NOT a physical characteristic of
-                  primates?
-                </p>
-
-                <form>
-                  <div className="radio">
-                    <label htmlFor="locomotion">
-                      <input type="radio" value="option1" />
-                      locomotion
-                    </label>
-                  </div>
-                  <div className="radio">
-                    <label htmlFor="nose">
-                      <input type="radio" value="option2" />
-                      nose
-                    </label>
-                  </div>
-                  <div className="radio">
-                    <label htmlFor="humor">
-                      <input type="radio" value="option3" />
-                      humor
-                    </label>
-                  </div>
-                  <div className="radio">
-                    <label htmlFor="eyesight">
-                      <input type="radio" value="option3" />
-                      eyesight
-                    </label>
-                  </div>
-                  <br />
-                  <Button onClick={this.close} negative>
-                    I dont know
-                  </Button>
-                </form>
-              </Modal.Description>
-            </Modal.Content>
-          </div>
-        </Modal>
         <div>
-          <Link to={routes.COUNTER}>to Counter</Link>
+          <p>Please enter a subject ID, then choose a video set:</p>
+        </div>
+        <div>
+          <p>
+            <Input placeholder="Subject ID" />
+          </p>
+        </div>
+        <div>
+          <p>
+            <Link to={routes.VIDEOSET}>Video Set #1</Link>
+          </p>
+        </div>
+        <div>
+          <p>
+            <Link to={routes.VIDEOSET}>Video Set #2</Link>
+          </p>
+        </div>
+        <div>
+          <p>
+            <Link to={routes.VIDEOSET}>Video Set #3</Link>
+          </p>
+        </div>
+        <div>
+          <p>
+            <Link to={routes.VIDEOSET}>Video Set #4</Link>
+          </p>
         </div>
       </div>
     );
