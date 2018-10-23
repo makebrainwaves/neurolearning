cd452b5213ae3278e309dc0204567c371a6830ba
Alex H <alexandra.higuera@gmail.com>
2018-10-23 17:46:59 -0400

    Prelim question updates

--- app/questions/questionsArray.js
+++ app/questions/questionsArray.js
@@ -3,29 +3,29 @@ module.exports = [
     key: 'q1',
     name: 'Question 1:',
     question:
-      '1Which one of the following is NOT a physical characteristic of primates?',
-    option1: 'Locomotion',
-    option2: 'Nose',
-    option3: 'Humor',
-    option4: 'Eyesight',
+      'A Niche is:',
+    option1: 'The Forest',
+    option2: 'The role an organism plays in the habitat',
+    option3: 'A small space',
+    option4: 'The habitat',
     option5: "I don't know"
   },
   {
     key: 'q2',
     name: 'Question 2:',
     question:
-      '2Which one of the following is NOT a physical characteristic of primates?',
-    option1: 'Locomotion',
-    option2: 'Nose',
-    option3: 'Humor',
-    option4: 'Eyesight',
+      'A habitat is:',
+    option1: 'A physical place',
+    option2: 'A function or activity',
+    option3: 'A closed place',
+    option4: 'A theoretical place',
     option5: "I don't know"
   },
   {
     key: 'q3',
     name: 'Question 3:',
     question:
-      '3Which one of the following is NOT a physical characteristic of primates?',
+      'The arctic tundra will provide organisms with:',
     option1: 'Locomotion',
     option2: 'Nose',
     option3: 'Humor',
