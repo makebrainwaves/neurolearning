f7ad5e861924e9cb3dd4269b3cc595fa602eae7c
Alex H <alexandra.higuera@gmail.com>
2018-11-04 19:38:50 -0500

    Fix modal display issue; Only display button if option selected

--- app/components/VideoComponents/VideoSet.js
+++ app/components/VideoComponents/VideoSet.js
@@ -32,10 +32,11 @@ interface State {
   firstOption: string;
   secondOption: string;
   thirdOption: string;
-  fourthOption: string;
-  fifthOption: string;
   answerQ1: string;
   answerQ2: string;
+  answerQ3: string;
+  answerQ4: string;
+  obscureButton: boolean;
 }
 
 interface Props {
@@ -80,6 +81,7 @@ export default class VideoSet extends Component<Props, State> {
 
     this.state = {
       isRunning: false,
+      modalIsOpen: false,
       question1AlreadyShown: false,
       question2AlreadyShown: false,
       questionNumber: '',
@@ -87,13 +89,40 @@ export default class VideoSet extends Component<Props, State> {
       firstOption: '',
       secondOption: '',
       thirdOption: '',
-      fourthOption: '',
-      fifthOption: '',
-      answerQ1: 'option5',
-      answerQ2: 'option5',
+      answers: {
+        niches: {
+          q1: '',
+          q2: '',
+          q3: '',
+          q4: ''
+        },
+        lipid: {
+          q1: '',
+          q2: '',
+          q3: '',
+          q4: ''
+        },
+        bip: {
+          q1: '',
+          q2: '',
+          q3: '',
+          q4: ''
+        },
+        insulin: {
+          q1: '',
+          q2: '',
+          q3: '',
+          q4: ''
+        }
+      },
+      answerQ1: '',
+      answerQ2: '',
+      answerQ3: '',
+      answerQ4: '',
       classifierEEGObservable,
       classifierScore: 0,
-      classifierThreshold: 1.1 // TODO: set this based on baseline data collection
+      classifierThreshold: 1.1,
+      obscureButton: true // TODO: set this based on baseline data collection
     };
     // These are just so that we can unsubscribe from the observables
     this.rawEEGSubscription = null;
@@ -106,7 +135,8 @@ export default class VideoSet extends Component<Props, State> {
 
   componentWillMount() {
     this.setState({
-      currentVideo: this.props.location.state.firstVideo
+      currentVideo: this.props.location.state.firstVideo,
+      questionSet: this.state.questionSet
     });
   }
   componentDidMount() {
@@ -121,13 +151,14 @@ export default class VideoSet extends Component<Props, State> {
     }
   }
 
-  closeModal = () =>
+  closeModal = () => {
     this.setState({
-      modalIsOpen: false
+      modalIsOpen: false,
+      obscureButton: true
     });
+  };
 
   playVideo = videoSequence => {
-    console.log('videoSequence', videoSequence);
     const videoRef = this.getVideoRef;
     videoRef.play();
     this.setState({ isRunning: true });
@@ -141,13 +172,10 @@ export default class VideoSet extends Component<Props, State> {
 
   newNextQuestion = key => {
     const videoQuestions = this.getQuestionSet(this.state.currentVideo);
-    this.pauseVideo();
-    console.log('videoQuestions', videoQuestions);
 
     for (let i = 0; i < videoQuestions.length; i++) {
       if (videoQuestions[i].key === key) {
-        console.log('videoQuestions ey', videoQuestions[i].key);
-        console.log('videoQuestions value', videoQuestions[i].value);
+        this.pauseVideo();
         this.setState({
           questionNumber: videoQuestions[i].key,
           questionText: videoQuestions[i].value.question,
@@ -180,38 +208,44 @@ export default class VideoSet extends Component<Props, State> {
   };
 
   getQuestionSet(video) {
-    let questionSet = [];
+    let questionSetTemp = [];
     if (
       video ===
       'http://localhost:1212/dist/0aaa1f67050e199bf65b346ed1e6bddf.mp4'
     ) {
-      questionSet = nichesQ;
+      questionSetTemp = nichesQ;
     } else if (
       video ===
       'http://localhost:1212/dist/2ab8ce87a09d1d6b7303006753ca0251.mp4'
     ) {
-      questionSet = lipidQ;
+      questionSetTemp = lipidQ;
     } else if (
       video ===
       'http://localhost:1212/dist/0b30e12cf7d23e654b6d6c306bd13618.mp4'
     ) {
-      questionSet = bipQ;
+      questionSetTemp = bipQ;
     } else if (
       video ===
       'http://localhost:1212/dist/a6e5c47df7b77a974f47cce5b094f90c.mp4'
     ) {
-      questionSet = insulinQ;
+      questionSetTemp = insulinQ;
     } else {
-      questionSet = null;
+      questionSetTemp = null;
     }
-    return questionSet;
+    this.setState({ questionSet: questionSetTemp });
+
+    return questionSetTemp;
   }
 
   onTimeUpdate = () => {
     const {
       question1AlreadyShown,
       question2AlreadyShown,
-      isRunning
+      isRunning,
+      answerQ1,
+      answerQ2,
+      answerQ3,
+      answerQ4
     } = this.state;
 
     const vidCurrTime = document.getElementById('vidID').currentTime;
@@ -235,17 +269,18 @@ export default class VideoSet extends Component<Props, State> {
       }
     } else {
       // control:
+
       switch (true) {
-        case 4 <= vidCurrTime && vidCurrTime < 8:
+        case 4 <= vidCurrTime && vidCurrTime < 8 && answerQ1 === '':
           this.newNextQuestion(1);
           break;
-        case 8 <= vidCurrTime && vidCurrTime < 10:
+        case 8 <= vidCurrTime && vidCurrTime < 10 && answerQ2 === '':
           this.newNextQuestion(2);
           break;
-        case 100 <= vidCurrTime && vidCurrTime < 120:
+        case 10 <= vidCurrTime && vidCurrTime < 12 && answerQ3 === '':
           this.newNextQuestion(3);
           break;
-        case 120 <= vidCurrTime && vidCurrTime < 140:
+        case 12 <= vidCurrTime && vidCurrTime < 14 && answerQ4 === '':
           this.newNextQuestion(4);
           break;
         default:
@@ -255,29 +290,30 @@ export default class VideoSet extends Component<Props, State> {
   };
 
   generateCsvs = videoSequence => {
-    console.log('your video has ended with sequence', videoSequence);
-    console.log('your current video at end: ', this.state.currentVideo);
-
     if (this.state.currentVideo === this.props.location.state.firstVideo) {
       this.setState({
-        currentVideo: this.props.location.state.secondVideo
+        currentVideo: this.props.location.state.secondVideo,
+        questionSet: this.getQuestionSet(this.props.location.state.secondVideo)
       });
     } else if (
       this.state.currentVideo === this.props.location.state.secondVideo
     ) {
       this.setState({
-        currentVideo: this.props.location.state.thirdVideo
+        currentVideo: this.props.location.state.thirdVideo,
+        questionSet: this.getQuestionSet(this.props.location.state.thirdVideo)
       });
     } else if (
       this.state.currentVideo === this.props.location.state.thirdVideo
     ) {
       this.setState({
-        currentVideo: this.props.location.state.fourthVideo
+        currentVideo: this.props.location.state.fourthVideo,
+        questionSet: this.getQuestionSet(this.props.location.state.fourthVideo)
       });
     }
   };
 
   handleQuestion(q, e) {
+    this.setState({ obscureButton: false });
     console.log(
       'this is the question you have just answered: ',
       q.questionNumber
@@ -291,13 +327,25 @@ export default class VideoSet extends Component<Props, State> {
     if (q.questionNumber === 2) {
       this.setState({ answerQ2: e.target.value });
     }
+
+    if (q.questionNumber === 3) {
+      this.setState({ answerQ3: e.target.value });
+    }
+
+    if (q.questionNumber === 4) {
+      this.setState({ answerQ4: e.target.value });
+    }
   }
 
   render() {
     const {
       modalIsOpen,
+      questionSet,
+      answers,
       answerQ1,
       answerQ2,
+      answerQ3,
+      answerQ4,
       questionNumber,
       questionText,
       firstOption,
@@ -403,69 +451,71 @@ export default class VideoSet extends Component<Props, State> {
           </div>
         </div>
 
-        {modalIsOpen && (
-          <Modal
-            open={modalIsOpen}
-            className={styles.modal}
-            closeOnEscape={false}
-            closeOnDimmerClick={false}
-          >
-            <div className={styles.inner}>
-              <Modal.Header>{questionNumber}</Modal.Header>
-              <Modal.Content className={styles.content}>
-                <Modal.Description>
-                  <p>{questionText}</p>
-
-                  <div>
-                    <div className="radio">
-                      <label htmlFor={firstOption}>
-                        <input
-                          name="option"
-                          type="radio"
-                          value="option1"
-                          onChange={e =>
-                            this.handleQuestion({ questionNumber }, e)
-                          }
-                        />
-                        {firstOption}
-                      </label>
-                    </div>
-                    <div className="radio">
-                      <label htmlFor={secondOption}>
-                        <input
-                          name="option"
-                          type="radio"
-                          value="option2"
-                          onChange={e =>
-                            this.handleQuestion({ questionNumber }, e)
-                          }
-                        />
-                        {secondOption}
-                      </label>
-                    </div>
-                    <div className="radio">
-                      <label htmlFor={thirdOption}>
-                        <input
-                          name="option"
-                          type="radio"
-                          value="option3"
-                          onChange={e =>
-                            this.handleQuestion({ questionNumber }, e)
-                          }
-                        />
-                        {thirdOption}
-                      </label>
-                    </div>
-                    <br />
+        <Modal
+          open={this.state.modalIsOpen}
+          className={styles.modal}
+          closeOnEscape={false}
+          closeOnDimmerClick={false}
+        >
+          <div className={styles.inner}>
+            <Modal.Header>{this.state.questionNumber}</Modal.Header>
+            <Modal.Content className={styles.content}>
+              <Modal.Description>
+                <p>{this.state.questionText}</p>
+                <p>
+                  <h5>Please select an answer to continue</h5>
+                </p>
+                <div>
+                  <div className="radio">
+                    <label htmlFor={this.state.firstOption}>
+                      <input
+                        name="option"
+                        type="radio"
+                        value="option1"
+                        onChange={e =>
+                          this.handleQuestion({ questionNumber }, e)
+                        }
+                      />
+                      {this.state.firstOption}
+                    </label>
+                  </div>
+                  <div className="radio">
+                    <label htmlFor={this.state.secondOption}>
+                      <input
+                        name="option"
+                        type="radio"
+                        value="option2"
+                        onChange={e =>
+                          this.handleQuestion({ questionNumber }, e)
+                        }
+                      />
+                      {this.state.secondOption}
+                    </label>
+                  </div>
+                  <div className="radio">
+                    <label htmlFor={this.state.thirdOption}>
+                      <input
+                        name="option"
+                        type="radio"
+                        value="option3"
+                        onChange={e =>
+                          this.handleQuestion({ questionNumber }, e)
+                        }
+                      />
+                      {this.state.thirdOption}
+                    </label>
+                  </div>
+                  <br />
+                  {!this.state.obscureButton && (
                     <Button onClick={this.closeModal} type="submit">
                       Submit
                     </Button>
-                  </div>
-                </Modal.Description>
-              </Modal.Content>
-            </div>
-          </Modal>
-        )}
+                  )}
+                </div>
+              </Modal.Description>
+            </Modal.Content>
+          </div>
+        </Modal>
       </div>
     );
   }
