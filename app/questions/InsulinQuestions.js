e1b5aad99d8a60a9c3bd88bf7d6263a4000b154d
Alex H <alexandra.higuera@gmail.com>
2018-12-03 16:19:10 -0500

    Update question sets

--- app/questions/InsulinQuestions.js
+++ app/questions/InsulinQuestions.js
@@ -3,7 +3,7 @@ module.exports = [
     key: 1,
     value: {
       name: 'Question 1:',
-      period: 'first20Seconds',
+      period: 80, // '1:00-1:20' or 60-80
       question: 'insulin1true or false?',
       option1: 'True',
       option2: 'False',
@@ -14,7 +14,7 @@ module.exports = [
     key: 2,
     value: {
       name: 'Question 2:',
-      period: 'second20Seconds',
+      period: 100, // '1:20-1:40' or 80-100
       question: 'insulin2true or false?',
       option1: 'True',
       option2: 'False',
@@ -25,7 +25,7 @@ module.exports = [
     key: 3,
     value: {
       name: 'Question 3:',
-      period: 'third20Seconds',
+      period: 120, // '1:40-2:00' or 100-120
       question: 'insulin3true or false?',
       option1: 'True',
       option2: 'False',
@@ -36,7 +36,7 @@ module.exports = [
     key: 4,
     value: {
       name: 'Question 4:',
-      period: 'fourth20Seconds',
+      period: 140, // '2:00-2:20' or 120-140
       question: 'insulin4true or false?',
       option1: 'True',
       option2: 'False',
@@ -47,7 +47,7 @@ module.exports = [
     key: 5,
     value: {
       name: 'Question 5:',
-      period: 'fifth20Seconds',
+      period: 160, // '2:20-2:40' or 140-160
       question: 'insulin5true or false?',
       option1: 'True',
       option2: 'False',
@@ -58,8 +58,140 @@ module.exports = [
     key: 6,
     value: {
       name: 'Question 6:',
-      period: 'sixth20Seconds',
+      period: 180, // '2:40-3:00' or 160-180
       question: 'insulin6true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 7,
+    value: {
+      name: 'Question 7:',
+      period: 200, // '3:00-3:20' or 180-200
+      question: 'insulin7true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 8,
+    value: {
+      name: 'Question 8:',
+      period: 220, // '3:20-3:40' or 200-220
+      question: 'insulin8true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 9,
+    value: {
+      name: 'Question 9:',
+      period: 240, // '3:40-4:00' or 220-240
+      question: 'insulin9true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 10,
+    value: {
+      name: 'Question 10:',
+      period: 260, // '4:00-4:20' or 240-260
+      question: 'insulin10true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 11,
+    value: {
+      name: 'Question 11:',
+      period: 280, // '4:20-4:40' or 260-280
+      question: 'insulin11true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 12,
+    value: {
+      name: 'Question 12:',
+      period: 300, // '4:40-5:00' or 280-300
+      question: 'insulin12true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 13,
+    value: {
+      name: 'Question 13:',
+      period: 320, // '5:00-5:20' or 300-320
+      question: 'insulin13true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 14,
+    value: {
+      name: 'Question 14:',
+      period: 340, // '5:20-5:40' or 320-340
+      question: 'insulin14true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 15,
+    value: {
+      name: 'Question 15:',
+      period: 360, // '5:40-6:00' or 340-360
+      question: 'insulin15true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 16,
+    value: {
+      name: 'Question 16:',
+      period: 380, // '6:00-6:20' or 360-380
+      question: 'insulin16true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 17,
+    value: {
+      name: 'Question 17:',
+      period: 400, // '6:20-6:40' or 380-400
+      question: 'insulin17true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 18,
+    value: {
+      name: 'Question 18:',
+      period: 420, // '6:40-7:00' or 400-420
+      question: 'insulin18true or false?',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
--- app/questions/LipidQuestions.js
+++ app/questions/LipidQuestions.js
@@ -3,8 +3,8 @@ module.exports = [
     key: 1,
     value: {
       name: 'Question 1:',
-      period: 'first20Seconds',
-      question: 'lipids1true or false?',
+      period: 80, // '1:00-1:20' or 60-80
+      question: 'lipid1true or false?',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -14,8 +14,8 @@ module.exports = [
     key: 2,
     value: {
       name: 'Question 2:',
-      period: 'second20Seconds',
-      question: 'lipids2true or false?',
+      period: 100, // '1:20-1:40' or 80-100
+      question: 'lipid2true or false?',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -25,8 +25,8 @@ module.exports = [
     key: 3,
     value: {
       name: 'Question 3:',
-      period: 'third20Seconds',
-      question: 'lipids3true or false?',
+      period: 120, // '1:40-2:00' or 100-120
+      question: 'lipid3true or false?',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -36,8 +36,8 @@ module.exports = [
     key: 4,
     value: {
       name: 'Question 4:',
-      period: 'fourth20Seconds',
-      question: 'lipids4true or false?',
+      period: 140, // '2:00-2:20' or 120-140
+      question: 'lipid4true or false?',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -47,8 +47,8 @@ module.exports = [
     key: 5,
     value: {
       name: 'Question 5:',
-      period: 'fifth20Seconds',
-      question: 'lipids5true or false?',
+      period: 160, // '2:20-2:40' or 140-160
+      question: 'lipid5true or false?',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -58,8 +58,140 @@ module.exports = [
     key: 6,
     value: {
       name: 'Question 6:',
-      period: 'sixth20Seconds',
-      question: 'lipids6true or false?',
+      period: 180, // '2:40-3:00' or 160-180
+      question: 'lipid6true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 7,
+    value: {
+      name: 'Question 7:',
+      period: 200, // '3:00-3:20' or 180-200
+      question: 'lipid7true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 8,
+    value: {
+      name: 'Question 8:',
+      period: 220, // '3:20-3:40' or 200-220
+      question: 'lipid8true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 9,
+    value: {
+      name: 'Question 9:',
+      period: 240, // '3:40-4:00' or 220-240
+      question: 'lipid9true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 10,
+    value: {
+      name: 'Question 10:',
+      period: 260, // '4:00-4:20' or 240-260
+      question: 'lipid10true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 11,
+    value: {
+      name: 'Question 11:',
+      period: 280, // '4:20-4:40' or 260-280
+      question: 'lipid11true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 12,
+    value: {
+      name: 'Question 12:',
+      period: 300, // '4:40-5:00' or 280-300
+      question: 'lipid12true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 13,
+    value: {
+      name: 'Question 13:',
+      period: 320, // '5:00-5:20' or 300-320
+      question: 'lipid13true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 14,
+    value: {
+      name: 'Question 14:',
+      period: 340, // '5:20-5:40' or 320-340
+      question: 'lipid14true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 15,
+    value: {
+      name: 'Question 15:',
+      period: 360, // '5:40-6:00' or 340-360
+      question: 'lipid15true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 16,
+    value: {
+      name: 'Question 16:',
+      period: 380, // '6:00-6:20' or 360-380
+      question: 'lipid16true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 17,
+    value: {
+      name: 'Question 17:',
+      period: 400, // '6:20-6:40' or 380-400
+      question: 'lipid17true or false?',
+      option1: 'True',
+      option2: 'False',
+      option3: "I don't know"
+    }
+  },
+  {
+    key: 18,
+    value: {
+      name: 'Question 18:',
+      period: 420, // '6:40-7:00' or 400-420
+      question: 'lipid18true or false?',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
