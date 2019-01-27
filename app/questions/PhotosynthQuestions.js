db45af3ac982864e2c95374a96b160838681c29b
Alex H <alexandra.higuera@gmail.com>
2019-01-27 17:20:05 -0500

    Update fuel and photosynth questions

--- app/questions/PhotosynthQuestions.js
+++ app/questions/PhotosynthQuestions.js
@@ -5,7 +5,7 @@ module.exports = [
       name: 'Question 1:',
       period: 80, // '1:00-1:20' or 60-80
       question:
-        'photosynth1true or false? Photosynthetic organisms produce their own sugars using photosynthesis and they do not really need O2 as much as they need CO2',
+        'True or false? Photosynthetic organisms produce their own sugars using photosynthesis and they do not really need O2 as much as they need CO2',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -17,7 +17,7 @@ module.exports = [
       name: 'Question 2:',
       period: 100, // '1:20-1:40' or 80-100
       question:
-        'photosynth2true or false? Insects tend to produce oils in the form of fats.',
+        'True or false? Insects tend to produce oils in the form of fats.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -29,7 +29,7 @@ module.exports = [
       name: 'Question 3:',
       period: 120, // '1:40-2:00' or 100-120
       question:
-        'photosynth3true or false? Animals play no role in the biological conversion of biomass into useful chemicals and fuels',
+        'True or false? Animals play no role in the biological conversion of biomass into useful chemicals and fuels',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -41,7 +41,7 @@ module.exports = [
       name: 'Question 4:',
       period: 140, // '2:00-2:20' or 120-140
       question:
-        'photosynth4true or false? Algae only need the sun to produce their own sugars; thus, if fed fully formed sugars, they have no need for sunlight.',
+        'True or false? Algae only need the sun to produce their own sugars; thus, if fed fully formed sugars, they have no need for sunlight.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -53,7 +53,7 @@ module.exports = [
       name: 'Question 5:',
       period: 160, // '2:20-2:40' or 140-160
       question:
-        'photosynth5true or false? Martek and Solazyme are two companies that have master the technique of growing algae in the dark for oil harvesting purposes.',
+        'True or false? Martek and Solazyme are two companies that have master the technique of growing algae in the dark for oil harvesting purposes.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -65,7 +65,7 @@ module.exports = [
       name: 'Question 6:',
       period: 180, // '2:40-3:00' or 160-180
       question:
-        'photosynth6true or false? Martek and Solazyme are two companies that have mastered the technique of growing algae indoors.',
+        'True or false? Martek and Solazyme are two companies that have mastered the technique of growing algae indoors.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -77,7 +77,7 @@ module.exports = [
       name: 'Question 7:',
       period: 200, // '3:00-3:20' or 180-200
       question:
-        'photosynth7true or false? Growing algae in the light is the most popular way to grow algae.',
+        'True or false? Growing algae in the light is the most popular way to grow algae.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -89,7 +89,7 @@ module.exports = [
       name: 'Question 8:',
       period: 220, // '3:20-3:40' or 200-220
       question:
-        'photosynth8true or false? The vast majority of algae grown in the light are used as food and for nutritional supplements.',
+        'True or false? The vast majority of algae grown in the light are used as food and for nutritional supplements.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -101,7 +101,7 @@ module.exports = [
       name: 'Question 9:',
       period: 240, // '3:40-4:00' or 220-240
       question:
-        'photosynth9true or false? Blue-green algae can excrete ethanol into the water around it.',
+        'True or false? Blue-green algae can excrete ethanol into the water around it.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -113,7 +113,7 @@ module.exports = [
       name: 'Question 10:',
       period: 260, // '4:00-4:20' or 240-260
       question:
-        'photosynth10true or false? Algenol uses algae that excrete ethanol into the water around it.',
+        'True or false? Algenol uses algae that excrete ethanol into the water around it.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -125,7 +125,7 @@ module.exports = [
       name: 'Question 11:',
       period: 280, // '4:20-4:40' or 260-280
       question:
-        'photosynth11true or false? Joule Unlimited aims to use cyanobacteria that excretes fatty acids and other diesel precursors into the water around them.',
+        'True or false? Joule Unlimited aims to use cyanobacteria that excretes fatty acids and other diesel precursors into the water around them.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -136,8 +136,7 @@ module.exports = [
     value: {
       name: 'Question 12:',
       period: 300, // '4:40-5:00' or 280-300
-      question:
-        'photosynth12true or false? Trees are not able to generate fuels.',
+      question: 'True or false? Trees are not able to generate fuels.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -149,7 +148,7 @@ module.exports = [
       name: 'Question 13:',
       period: 320, // '5:00-5:20' or 300-320
       question:
-        'photosynth13true or false? PETRO stands for Protazo Engineered to Replace Oil.',
+        'True or false? PETRO stands for Protazo Engineered to Replace Oil.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -161,7 +160,7 @@ module.exports = [
       name: 'Question 14:',
       period: 340, // '5:20-5:40' or 320-340
       question:
-        'photosynth14true or false? Oilseed crops are the best example for plants being used for biological conversion.',
+        'True or false? Oilseed crops are the best example for plants being used for biological conversion.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -173,7 +172,7 @@ module.exports = [
       name: 'Question 15:',
       period: 360, // '5:40-6:00' or 340-360
       question:
-        'photosynth15true or false? The extraction of oils from oilseed crops is a biological conversion.',
+        'True or false? The extraction of oils from oilseed crops is a biological conversion.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -185,7 +184,7 @@ module.exports = [
       name: 'Question 16:',
       period: 380, // '6:00-6:20' or 360-380
       question:
-        'photosynth16true or false? Tunicate is one of the only known animals on earth that produces cellulose.',
+        'True or false? Tunicate is one of the only known animals on earth that produces cellulose.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -196,8 +195,7 @@ module.exports = [
     value: {
       name: 'Question 17:',
       period: 400, // '6:20-6:40' or 380-400
-      question:
-        'photosynth17true or false? Algae is not able to create cellulose.',
+      question: 'True or false? Algae is not able to create cellulose.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -209,7 +207,7 @@ module.exports = [
       name: 'Question 18:',
       period: 420, // '6:40-7:00' or 400-420
       question:
-        'photosynth18true or false? We have been using petroleum much longer than we have been using animal fats for oil. ',
+        'True or false? We have been using petroleum much longer than we have been using animal fats for oil. ',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -221,7 +219,7 @@ module.exports = [
       name: 'Question 19:',
       period: 440, // '7:00-7:20' or 420-440
       question:
-        'photosynth19true or false? Animal fat is a major source of biologically available oils for bioenergy.',
+        'True or false? Animal fat is a major source of biologically available oils for bioenergy.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -233,7 +231,7 @@ module.exports = [
       name: 'Question 20:',
       period: 460, // '7:20-7:40' or 440-460
       question:
-        'photosynth20true or false? The amount of animal fat produced in the U.S. is far greater than the equivalent of what the largest oil field would produce in a given year.',
+        'True or false? The amount of animal fat produced in the U.S. is far greater than the equivalent of what the largest oil field would produce in a given year.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -245,7 +243,7 @@ module.exports = [
       name: 'Question 21:',
       period: 480, // '7:40-8:00' or 460-480
       question:
-        'photosynth21true or false? Insects are not a source of oil - they only consume it.',
+        'True or false? Insects are not a source of oil - they only consume it.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -257,7 +255,7 @@ module.exports = [
       name: 'Question 22:',
       period: 500, // '8:00-8:20' or 480-500
       question:
-        'photosynth22true or false? There are no industries using insects’ chemicals today.',
+        'True or false? There are no industries using insects’ chemicals today.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -269,7 +267,7 @@ module.exports = [
       name: 'Question 23:',
       period: 520, // '8:20-8:40' or 500-520
       question:
-        'photosynth23true or false? The use of insects as source of useable oil is a process that requires an urban environment.',
+        'True or false? The use of insects as source of useable oil is a process that requires an urban environment.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -282,7 +280,7 @@ module.exports = [
       name: 'Question 24:',
       period: 540, // '8:40-9:00' or 520-540
       question:
-        'photosynth24true or false? Biological conversions often make chemicals easy for humans to synthesize.',
+        'True or false? Biological conversions often make chemicals easy for humans to synthesize.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -294,7 +292,7 @@ module.exports = [
       name: 'Question 25:',
       period: 560, // '9:00-9:20' or 540-560
       question:
-        'photosynth25true or false? Genetic engineering is rapidly increasing the number of organisms that secrete valuable products.',
+        'True or false? Genetic engineering is rapidly increasing the number of organisms that secrete valuable products.',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -306,7 +304,7 @@ module.exports = [
       name: 'Question 26:',
       period: 580, // '9:20-9:40' or 560-580
       question:
-        'photosynth26true or false? The organism shown in the picture is made up of just one cell. ',
+        'True or false? The organism shown in the picture is made up of just one cell. ',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
@@ -317,8 +315,7 @@ module.exports = [
     value: {
       name: 'Question 27:',
       period: 600, // '9:40-10:00' or 580-600
-      question:
-        'photosynth27true or false? Cells are always invisible to the naked eye. ',
+      question: 'True or false? Cells are always invisible to the naked eye. ',
       option1: 'True',
       option2: 'False',
       option3: "I don't know"
