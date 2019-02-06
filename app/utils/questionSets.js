ee1631122cc952e52c92fc952a33cb23a070c8e8
Alex H <alexandra.higuera@gmail.com>
2019-02-06 11:00:47 -0500

    questionSet cleanup

--- app/utils/questionSets.js
+++ app/utils/questionSets.js
@@ -17,55 +17,9 @@ const photosynthQ = require('../questions/PhotosynthQuestions.js');
 interface State {
   questionSet: string;
 }
-/*
-export const removeNoise = (threshold: number = VARIANCE_THRESHOLD) =>
-  pipe(
-    deMean(),
-    addSignalQuality(),
-    filter(epo => {
-      const isNoiseArray = Object.values(epo.signalQuality).map(
-        noise => (noise >= threshold ? false : true)
-      );
-      // console.log(isNoiseArray);
-      return isNoiseArray.reduce((acc, curr) => (curr ? curr : acc), true);
-    })
-  );
 
-export const computeAlpha = (alphaRange: Array<number> = [8, 13]) =>
-  pipe(
-    fft({ bins: FFT_BINS }),
-    powerByBand({ alpha: alphaRange }),
-    map(bandPowers => bandPowers.alpha)
-  );
+/* getQuestionSet - returns the original question set */
 
-export const computeThetaBeta = (
-  thetaRange: Array<number> = [4, 7.5],
-  betaRange: Array<number> = [12.5, 30]
-) =>
-  pipe(
-    fft({ bins: FFT_BINS }),
-    powerByBand({ theta: thetaRange, beta: betaRange }),
-    map(bandPowers =>
-      bandPowers.theta.map(
-        (channelTheta, index) => channelTheta / bandPowers.beta[index]
-      )
-    )
-  );
-
-export const deMean = () =>
-  pipe(
-    map(epo => {
-      const channelMeans = epo.data.map(average);
-      return {
-        ...epo,
-        data: epo.data.map((channelData, index) =>
-          channelData.map(value => value - channelMeans[index])
-        )
-      };
-    })
-  );
-
-  */
 export const getQuestionSet = video => {
   let questionSetTemp = [];
   if (video === biomassVideo) {
@@ -81,4 +35,101 @@ export const getQuestionSet = video => {
   }
 
   return questionSetTemp;
+};
+
+/* getRandomQuestionSet - standard question structuring for control videos */
+
+export const getRandomQuestionSet = video => {
+  const videoQuestions = getQuestionSet(video);
+
+  let randomNumbers = [];
+  const arr = [];
+  const newQuestionSet = [];
+
+  // find random values between 1 and questionSet.length
+  while (arr.length < Math.floor(videoQuestions.length / 3)) {
+    const r = Math.floor(Math.random() * videoQuestions.length) + 1;
+    if (arr.indexOf(r) === -1) arr.push(r);
+  }
+
+  // sort array of random numbers
+  randomNumbers = arr.sort((a, b) => a - b);
+  // console.log('getRandomQuestionSet: randomrandomNumbers', randomNumbers);
+
+  // copy corresponding questions to new array
+  for (let i = 0; i < randomNumbers.length; i++) {
+    for (let j = 0; j < videoQuestions.length; j++) {
+      if (randomNumbers[i] === videoQuestions[j].key) {
+        newQuestionSet.push(videoQuestions[j]);
+      }
+    }
+  }
+  console.log('newQuestionSet - standard control', newQuestionSet);
+
+  return newQuestionSet;
+};
+
+/* getRandomControlQuestionSet - called when all 4 videos are control videos */
+export const getRandomControlQuestionSet = video => {
+  const videoQuestions = getQuestionSet(video);
+  let randomNumbers = [];
+  const arr = [];
+  const newQuestionSet = [];
+  // find random values between 1 and videoQuestions.length
+  while (arr.length < Math.floor((videoQuestions.length * 2) / 3)) {
+    const r = Math.floor(Math.random() * videoQuestions.length) + 1;
+    if (arr.indexOf(r) === -1) arr.push(r);
+  }
+
+  // sort array of random numbers
+  randomNumbers = arr.sort((a, b) => a - b);
+  console.log('randomrandomNumbers Control', randomNumbers);
+
+  // copy corresponding questions to new array
+  for (let i = 0; i < randomNumbers.length; i++) {
+    for (let j = 0; j < videoQuestions.length; j++) {
+      if (randomNumbers[i] === videoQuestions[j].key) {
+        newQuestionSet.push(videoQuestions[j]);
+      }
+    }
+  }
+  console.log('newQuestionSet - 4 Controls', newQuestionSet);
+
+  return newQuestionSet;
+};
+
+/* getRandomQuestionSetAfterExperimental - for when control video follows experimental */
+export const getControlQuestionstAfterExp = (video, numOfPrevExpQuestions) => {
+  const videoQuestions = getQuestionSet(video);
+
+  let randomNumbers = [];
+  const arr = [];
+  const newQuestionSet = [];
+
+  const videoQuestionsLength = Math.min(
+    numOfPrevExpQuestions,
+    videoQuestions.length
+  );
+
+  // find random values between 1 and videoQuestions.length
+  while (arr.length < Math.floor(videoQuestionsLength / 3)) {
+    const r = Math.floor(Math.random() * videoQuestionsLength) + 1;
+    if (arr.indexOf(r) === -1) arr.push(r);
+  }
+
+  // sort array of random numbers
+  randomNumbers = arr.sort((a, b) => a - b);
+  // console.log('randomNumbers(sortedarray) Control after Exp', randomNumbers);
+
+  // copy corresponding questions to new array
+  for (let i = 0; i < randomNumbers.length; i++) {
+    for (let j = 0; j < videoQuestions.length; j++) {
+      if (randomNumbers[i] === videoQuestions[j].key) {
+        newQuestionSet.push(videoQuestions[j]);
+      }
+    }
+  }
+  console.log('newQuestionSet - Control after Experimental', newQuestionSet);
+
+  return newQuestionSet;
 };
